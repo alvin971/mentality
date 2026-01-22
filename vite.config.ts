9a6958b0794ea807ad8973ff0757d2aa3b135130
alvin kuyo <loup.partner@gmail.com>
2026-01-22 17:02:36 -0400

    Update base URL to /mentality/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mentrality/', // Pour GitHub Pages
+  base: '/mentality/', // Pour GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
